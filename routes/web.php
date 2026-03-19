2d726da686b96f58c7fc55c82504eb424d95d84a
Olivier Colas <66916000+olimcfly@users.noreply.github.com>
2026-03-19 14:48:50 +0100

    feat: implement admin blog CRUD

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,14 @@ $router->get('/processus-estimation', [PageController::class, 'processusEstimati
 $router->get('/contact', [PageController::class, 'contact']);
 $router->get('/exemples-estimation', [PageController::class, 'exemplesEstimation']);
 $router->post('/contact', [PageController::class, 'contactSubmit']);
+
+$router->get('/blog', [BlogController::class, 'index']);
+$router->get('/blog/{slug}', [BlogController::class, 'show']);
+
+$router->get('/admin/blog', [AdminBlogController::class, 'index']);
+$router->get('/admin/blog/create', [AdminBlogController::class, 'create']);
+$router->post('/admin/blog/store', [AdminBlogController::class, 'store']);
+$router->get('/admin/blog/edit/{id}', [AdminBlogController::class, 'edit']);
+$router->post('/admin/blog/update/{id}', [AdminBlogController::class, 'update']);
+$router->post('/admin/blog/delete/{id}', [AdminBlogController::class, 'delete']);
+$router->post('/admin/blog/generate', [AdminBlogController::class, 'generate']);
